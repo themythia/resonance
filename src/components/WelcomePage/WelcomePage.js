267b9ee7bdb1721b239e00dff07c6f84b8501eda
AlexGioffDev <alex.gioffre_dev@outlook.it>
2021-11-19 14:32:20 +0100

    Little fix

--- src/components/WelcomePage/WelcomePage.js
+++ src/components/WelcomePage/WelcomePage.js
@@ -1,25 +1,31 @@
 import {
-  StyledWrapper, 
-  TopContainer, 
-  Logo, 
+  StyledWrapper,
+  TopContainer,
+  Logo,
   CenterContainer,
-  ButtonContainer} from '../../styled/Utils.js';
-
-
+  ButtonContainer,
+  PlayDiv,
+  SpotifyLogo,
+  Circle,
+  WaveRight,
+  WaveLeft} from "../../styled/Utils";
 const WelcomePage = () => {
-
-    return (
+  return (
     <StyledWrapper>
-     <TopContainer>
-         <Logo />
-          <p>Powered By *logo*</p>
+      <TopContainer>
+        <Logo />
+        <p>Powered By <SpotifyLogo /></p>
       </TopContainer>
       <CenterContainer>
+        <Circle />
+        <WaveLeft />
+        <WaveRight />
         <ButtonContainer>
+          <PlayDiv />         
         </ButtonContainer>
       </CenterContainer>
     </StyledWrapper>
-  )
-}
+  );
+};
 
 export default WelcomePage;
