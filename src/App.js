e381cdd1f6c497b9e03f75c4f71c3838001bf93d
Emir AL <mythia@windowslive.com>
2021-11-25 14:29:42 +0300

    Add: basic routing to App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,37 @@ import NowPlayingPage from './components/Player/NowPlayingPage';
 import WelcomePage from './components/WelcomePage/WelcomePage';
 import Login from './components/Login/Login.js';
 import PlaylistPage from './components/Playlist/PlaylistPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className='wrapper'>
-      {/* <NowPlayingPage /> */}
-      <WelcomePage />
-      {/* <PlaylistPage name='Chvrches Playlist' type='Playlist' creator='Emir' /> */}
-      {/* <Login /> */}
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<WelcomePage />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/home' element={<p>Placeholder home page</p>} />
+          <Route
+            exact
+            path='/library'
+            element={<p>Placeholder library page</p>}
+          />
+          <Route
+            path='/library/:playlistId'
+            element={
+              <PlaylistPage
+                name='Chvrches Playlist'
+                type='Playlist'
+                creator='Emir'
+              />
+            }
+          />
+        </Routes>
+      </Router>
+      {/* TO DO:
+      - create a private route
+      - create a route for now playing
+      */}
     </div>
   );
 }
