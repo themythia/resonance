0fbc34e13e824bab8b39f08b4cb34fe27b48e1df
Emir AL <mythia@windowslive.com>
2021-12-01 17:06:39 +0300

    Add: UserContext to handle user info on login

--- src/components/Authorize/Authorize.js
+++ src/components/Authorize/Authorize.js
@@ -1,19 +1,28 @@
-import {useEffect} from 'react';
+import { useEffect, useContext } from 'react';
 import request from 'request';
+import { UserContext } from '../../contexts/UserContext';
 
 const Authorize = () => {
-  
-  useEffect(()=> {
-    let access_token = window.location.hash.substring(1).split('access_token=')[1].split('&')[0];
+  const { setUserData } = useContext(UserContext);
+
+  useEffect(() => {
+    let access_token = window.location.hash
+      .substring(1)
+      .split('access_token=')[1]
+      .split('&')[0];
     var options = {
       url: 'https://api.spotify.com/v1/me',
-      headers: { 'Authorization': 'Bearer ' + access_token },
-      json: true
+      headers: { Authorization: 'Bearer ' + access_token },
+      json: true,
     };
 
-        // use the access token to access the Spotify Web API
-    request.get(options, function(error, response, body) {
+    // use the access token to access the Spotify Web API
+    request.get(options, function (error, response, body) {
       console.log(body);
+      setUserData({
+        isLoggedIn: true,
+        data: body,
+      });
     });
   }, []);
 
@@ -21,7 +30,7 @@ const Authorize = () => {
     <div>
       <h1>Hello</h1>
     </div>
-  )
-}
+  );
+};
 
 export default Authorize;
