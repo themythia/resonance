50b4b582fbd69aedb39e5f78c37e6f8cd780dd42
Mark Otweyo <markotweyo99@gmail.com>
2021-12-10 13:37:16 +0300

    Added placeholder on Search

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import {
   Input,
   HeaderContainer,
@@ -9,19 +9,33 @@ import { Search } from '@styled-icons/fluentui-system-filled';
 import { UserContext } from '../../contexts/UserContext';
 
 const Header = () => {
+  const [search, setSearch] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
   const { userData } = useContext(UserContext);
+  
+
+  useEffect(() => {
+    if (!search)
+     return setSearchResults([])
+    
+  }, [search])
+  
   return (
     <HeaderContainer>
       <SearchBarContainer>
-        <Input />
+        <Input 
+          type='search'
+          placeholder='search Songs/Artists' 
+          value={search}
+          onChange={(e)=>setSearch(e.target.value)}/>
         <Search size={24} />
       </SearchBarContainer>
-      <ProfilePic
+      {/*<ProfilePic
         src={
           userData.data.images?.[0].url || 'https://puu.sh/IsNdG/069cf308d1.png'
         }
         alt='user avatar'
-      />
+      />*/}
     </HeaderContainer>
   );
 };
