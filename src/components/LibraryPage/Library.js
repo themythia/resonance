b9780d7353e3a41a0c2ae438dccf0649c911556e
Emir AL <mythia@windowslive.com>
2021-12-10 15:51:17 +0300

    Update: placeholder playlistAlbums in Library

--- src/components/LibraryPage/Library.js
+++ src/components/LibraryPage/Library.js
@@ -7,62 +7,79 @@ import {
   StyledAlbumTextContainer,
 } from '../../styled/Library.styled';
 import logo from '../../logo.svg';
+import { Link } from 'react-router-dom';
 
 const playlistAlbums = [
   {
-    name: 'GymWorkout',
+    name: 'Track',
     description: 'headbanging metal',
     picture: logo,
+    type: 'albums',
+    id: '1oDHd8X6KrmLW3jFNsSfMu',
+    track: '39LUihkJzZGVAOCEAZayJb',
   },
   {
-    name: 'blink 182, smashing pumpkins',
+    name: 'Playlist',
     description: 'punk rock for retards',
     picture: logo,
+    type: 'playlists',
+    id: '2KqLYZ1ky6MR9VZb06bbb7',
   },
   {
-    name: 'GymWorkout2',
-    description: 'Djent and Brootalz',
-    picture: logo,
-  },
-  {
-    name: '90SlowJam',
+    name: 'Single',
     description: 'nice bedroom playlist',
     picture: logo,
+    type: 'albums',
+    id: '4OaKtwwLxQKHJTByixvKdF',
   },
   {
     name: 'Instrumentals',
     description: 'Guitar music',
     picture: logo,
+    type: 'albums',
+    id: '1oDHd8X6KrmLW3jFNsSfMu',
   },
   {
     name: 'OST1',
     description: 'favorite soundtracks',
     picture: logo,
+    type: 'playlists',
+    id: '2KqLYZ1ky6MR9VZb06bbb7',
   },
   {
     name: 'OST2',
     description: 'favorite soundtracks',
     picture: logo,
+    type: 'albums',
+    id: '1oDHd8X6KrmLW3jFNsSfMu',
   },
   {
     name: 'OST3',
     description: 'favorite soundtracks',
     picture: logo,
+    type: 'playlists',
+    id: '2KqLYZ1ky6MR9VZb06bbb7',
   },
   {
     name: 'OST4',
     description: 'favorite soundtracks',
     picture: logo,
+    type: 'albums',
+    id: '1oDHd8X6KrmLW3jFNsSfMu',
   },
   {
     name: 'ProgMetal',
     description: 'Evergrey and Symphony X for life!',
     picture: logo,
+    type: 'playlists',
+    id: '2KqLYZ1ky6MR9VZb06bbb7',
   },
   {
     name: 'Adriano Celentano',
     description: 'CAZZO!',
     picture: logo,
+    type: 'albums',
+    id: '1oDHd8X6KrmLW3jFNsSfMu',
   },
 ];
 
@@ -75,13 +92,19 @@ const Library = () => {
       <StyledAlbumSection>
         {playlistAlbums.map((item, index) => {
           return (
-            <StyledPlaylistItem key={index}>
-              <StyledAlbumThumbnail src='https://e.snmc.io/i/1200/s/0348449c729ad8e7082de82f30f90caf/3992350' />
-              <StyledAlbumTextContainer>
-                <p>{item.name}</p>
-                <h5>{item.description}</h5>
-              </StyledAlbumTextContainer>
-            </StyledPlaylistItem>
+            <Link
+              key={index}
+              to={`/library/${item.id}`}
+              state={{ type: item.type, track: item?.track }}
+            >
+              <StyledPlaylistItem>
+                <StyledAlbumThumbnail src='https://e.snmc.io/i/1200/s/0348449c729ad8e7082de82f30f90caf/3992350' />
+                <StyledAlbumTextContainer>
+                  <p>{item.name}</p>
+                  <h5>{item.description}</h5>
+                </StyledAlbumTextContainer>
+              </StyledPlaylistItem>
+            </Link>
           );
         })}
       </StyledAlbumSection>
