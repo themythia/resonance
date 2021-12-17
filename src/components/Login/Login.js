bab420d1c31b93c81272ecbe6e2ef23adb562e9b
Emir AL <mythia@windowslive.com>
2021-12-17 21:14:20 +0300

    Update: clientID and redirect URL

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -10,30 +10,27 @@ import {
 } from '../../styled/Utils';
 import querystring from 'querystring';
 
-
-
-
 const Login = () => {
-  
-
   const generateState = (lenState) => {
     let text = '';
-    let element = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let element =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     for (let i = 0; i < lenState; i++) {
       text += element.charAt(Math.floor(Math.random() * element.length));
     }
 
     return text;
-  }
+  };
 
-   const goToLogin = () => {
-
+  const goToLogin = () => {
     let state = generateState(16);
-    localStorage.setItem("stateValue", state)
-    let clientID = 'f573531847c5435e80f1ba528eceed98';
+    localStorage.setItem('stateValue', state);
+    let clientID = 'c3b87ffeb6ec4e7e927ae17346632264';
     let responsetype = 'token';
-    let scope = 'user-read-private user-read-email user-read-recently-played playlist-read-private playlist-modify-private playlist-modify-private';
-    let redirect_uri = 'http://localhost:3000/authorize';
+    let scope =
+      'user-read-private user-read-email user-read-recently-played playlist-read-private playlist-modify-private playlist-modify-private';
+    let redirect_uri =
+      'https://v35-geckos-team-06-qswe7y4e7-mythia.vercel.app/authorize';
     let url = `https://accounts.spotify.com/authorize?`;
     url += querystring.stringify({
       response_type: responsetype,
