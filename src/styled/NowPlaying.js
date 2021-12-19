95e91785e0645ee4a03ea8aa8002b0d12424118f
Emir AL <mythia@windowslive.com>
2021-12-19 11:10:41 +0300

    Fix: styled-components/macro imports

--- src/styled/NowPlaying.js
+++ src/styled/NowPlaying.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components/macro';
+import styled from 'styled-components';
 import { StyledIconBase } from '@styled-icons/styled-icon';
 
 export const NowPlayingContainer = styled.div`
